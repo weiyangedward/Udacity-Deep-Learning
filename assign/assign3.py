8c3546a42e7a7277af8849ad2d4ecab1bf699354
weiyangedward <weiyangedward@gmail.com>
2017-04-06 01:07:19 -0700

    add dropout and rate decay

--- assign/assign3.py
+++ assign/assign3.py
@@ -135,33 +135,17 @@ prob4
 """
 
 # Parameters
-learning_rate = 0.01
-training_epochs = 100
+# learning_rate = 0.5
+training_epochs = 1000
 batch_size = 128
 display_step = 1
 beta = 0.01
 hidden_nodes_1 = 1024
+start_learning_rate = 0.01
+keep_rate = 1
 
 """
-# tf Graph Input
-# x = tf.placeholder(tf.float32, [None, image_size*image_size])  # mnist data image of shape 28*28=784
-# y = tf.placeholder(tf.float32, [None, num_labels])  # 0-9 digits recognition => 10 classes  # Set model weights
-#
-# W_1 = tf.Variable(tf.truncated_normal([image_size * image_size, hidden_nodes_1],
-#                                       stddev=math.sqrt(2.0 / (image_size * image_size))))
-# b_1 = tf.Variable(tf.zeros([hidden_nodes_1]))
-# W_2 = tf.Variable(tf.truncated_normal([hidden_nodes_1, hidden_nodes_2], stddev=math.sqrt(2.0 / (hidden_nodes_1))))
-# b_2 = tf.Variable(tf.zeros([hidden_nodes_2]))
-# W_3 = tf.Variable(tf.truncated_normal([hidden_nodes_2, num_labels], stddev=math.sqrt(2.0 / (hidden_nodes_2))))
-# b_3 = tf.Variable(tf.zeros([num_labels]))
-
-# Minimize error using cross entropy
-# logits_1 = tf.nn.relu(tf.matmul(x, W_1) + b_1)
-# logits_2 = tf.nn.relu(tf.matmul(logits_1, W_2) + b_2)
-# logits_3 = tf.matmul(logits_2, W_3) + b_3
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits_3))
-# regularizer = tf.nn.l2_loss(W_1) + tf.nn.l2_loss(W_2) + tf.nn.l2_loss(W_3)
-# cost = tf.reduce_mean(cost + beta * regularizer)
+build deep-NN of any layer
 """
 feature_num = image_size*image_size
 layer_num = 5
@@ -170,20 +154,30 @@ x = tf.placeholder(tf.float32, [None, feature_num])  # mnist data image of shape
 y = tf.placeholder(tf.float32, [None, num_labels])  # 0-9 digits recognition => 10 classes  # Set model weights
 
 hidden_nodes = [int(hidden_nodes_1 * np.power(0.5, l)) for l in range(layer_num)]
-print('hidden_nodes = ', hidden_nodes)
 layer_sizes = [feature_num] + hidden_nodes + [num_labels]
-print('layer_sizes = ', layer_sizes)
 
 Ws = [tf.Variable(tf.truncated_normal([layer_sizes[l], layer_sizes[l + 1]], stddev=math.sqrt(2.0 / layer_sizes[l]))) for
       l in range(layer_num + 1)]
 bs = [tf.Variable(tf.zeros([layer_sizes[l]])) for l in range(1, layer_num + 2)]
 
+keep_prob = tf.placeholder(tf.float32)
+
+"""
+Problem 3
+Introduce Dropout on the hidden layer of the neural network. Remember:
+Dropout should only be introduced during training, not evaluation,
+otherwise your evaluation results would be stochastic as well.
+TensorFlow provides nn.dropout() for that, but you have to make sure it's only inserted during training.
+"""
 logits = x
 for k in range(layer_num):
-    logits = tf.nn.relu(tf.matmul(logits, Ws[k]) + bs[k])
-
+    logits = tf.nn.dropout(tf.nn.relu(tf.matmul(logits, Ws[k]) + bs[k]), keep_prob)
 logits = tf.matmul(logits, Ws[layer_num]) + bs[layer_num]
-print('logits = ', logits)
+
+pred_logits = x
+for k in range(layer_num):
+    pred_logits = tf.nn.relu(tf.matmul(pred_logits, Ws[k]) + bs[k])
+pred_logits = tf.matmul(pred_logits, Ws[layer_num]) + bs[layer_num]
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits))
 
@@ -215,12 +209,18 @@ print('hidden_nodes = ', len(hidden_nodes), 'layer_sizes = ', len(layer_sizes), 
 # regularizer = tf.nn.l2_loss(W_1) + tf.nn.l2_loss(W_2) + tf.nn.l2_loss(W_3)
 # cost = tf.reduce_mean(cost + beta * regularizer)
 
+"""
+use learning rate decay:
+"""
+global_step = tf.Variable(0)  # count the number of steps taken.
+learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, 100000, 0.96, staircase=True)
+
 # Gradient Descent
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost, global_step=global_step)
 
 # Construct model
 # pred = tf.nn.softmax(logits_3)  # Softmax
-pred = tf.nn.softmax(logits)
+pred = tf.nn.softmax(pred_logits)
 # Test model
 correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 # Calculate accuracy for 3000 examples
@@ -248,7 +248,7 @@ with tf.Session() as sess:
             #     print('i = ', i)
 
             # Fit training using batch data
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys})
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs, y: batch_ys, keep_prob: keep_rate})
             # Compute average loss
             avg_cost += c / total_batch
         # Display logs per epoch step
