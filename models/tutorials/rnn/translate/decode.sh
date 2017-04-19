e71c60ec0a4619572522f1c85b5f888fd7aad3bc
weiyangedward <weiyangedward@gmail.com>
2017-04-18 21:15:41 -0700

    add models

---
+++ models/tutorials/rnn/translate/decode.sh
@@ -0,0 +1,1 @@
+python3 translate.py --decode --data_dir data --train_dir checkpoints --size=1024 --num_layers=3 --max_train_data_size=22500000
---
+++ models/tutorials/rnn/translate/train.sh
@@ -0,0 +1,1 @@
+python3 -u translate.py --data_dir data --train_dir checkpoints --size=1024 --num_layers=3  --max_train_data_size=22500000 > train.log
